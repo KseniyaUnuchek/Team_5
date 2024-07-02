da9cb03b6e30e7c34191bb09dcf5dfe3f78a5bc3
KseniyaUnuchek <unuchekksusha@gmail.com>
2024-07-02 23:52:00 +0300

    ДАГ для проверки соединения с БД

---
+++ airflow-environment/dags/test_db_connection.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+import datetime
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.models import Variable
+
+config = Variable.get('time_config', deserialize_json=True) # {time_start: "* * * * 1"}
+
+dag = DAG('test_connection_to_source_db', description='test', schedule_interval=config['time_start'], start_date=datetime.datetime(2024,7,2), catchup=False)
+
+start_node = EmptyOperator(task_id="start_node", dag=dag)
+
+get_data = SQLExecuteQueryOperator(task_id="get_data",
+                            sql="SELECT * FROM source_data.инструменты",
+                            conn_id="source_db",
+                            dag=dag)
+
+start_node >> get_data
+
